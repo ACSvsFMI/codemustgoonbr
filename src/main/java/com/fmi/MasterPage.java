4cd7a17d7dda64a75c01dc67e8e87127de08d456
Iulian Mina <iuly_mina@yahoo.com>
2012-12-08 14:03:34 +0200

    link catre register

--- src/main/java/com/fmi/MasterPage.java
+++ src/main/java/com/fmi/MasterPage.java
@@ -1,13 +1,24 @@
 package com.fmi;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import account.Register;
 
 public class MasterPage extends WebPage {
 	FeedbackPanel feedback;
 	public MasterPage(PageParameters params) {
 		feedback = new FeedbackPanel("feedback");
 		add(feedback);
+		Link l = new Link("register") {
+
+			@Override
+			public void onClick() {
+				setResponsePage(Register.class);
+				
+			}
+		};
 	}
 }
